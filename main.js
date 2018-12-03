33709f7173ae37f6490d052951a214d75676790a
ACER <fauziaazia171097@gmail.com>
2018-12-03 02:57:20 -0500

    finish

--- main.js
+++ main.js
@@ -2,4 +2,7 @@
 
 const Calculator = require("./calculator.js")
 
+const Calculate = new Calculator(4)
+console.log(Calculate.add(2).divide(3).result)
+console.log(Calculate.circle())
 // execute function on calculator.js in here
