5a5b47d1bebfcdc03467842ac4ec4a199fd941c8
maitreya <bluespider529@gmail.com>
2018-12-03 22:12:35 +0700

    done advance calculator

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 'use strict'
 
 const Calculator = require("./calculator.js")
-
 // execute function on calculator.js in here
+const Calc = new Calculator();
+console.log(Calc.add(5).subtract(2).divide(3))
