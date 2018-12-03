f053621ec543ae6f170293930dbfb7b5a03a27d6
ShadowM8 <anton2014lenovo@gmail.com>
2018-12-03 11:01:51 +0700

    calculator done -Anton-

--- calculator.js
+++ calculator.js
@@ -2,21 +2,63 @@
 
 class Calculator {
   //write your code here
-  constructor () {
+  constructor (numberInput) {
+    this.input = numberInput
+    this.result = numberInput
+    this.tampilan = ''
+    // this.phiNumber = this.phi()
   }
-  add () {
+
+  add (inputAdd) {
+    this.result = this.result + inputAdd
+    this.tampilan = `hasil kalkulasi adalah ${this.result}`
+    return this
   }
-  subtract () {
+
+  subtract (inputSubstract) {
+    this.result = this.result - inputSubstract
+    this.tampilan = `hasil kalkulasi adalah ${this.result}`
+    return this
   }
-  multiply () {
+  multiply (inputMultiply) {
+    this.result = this.result * inputMultiply
+    this.tampilan = `hasil kalkulasi adalah ${this.result}`
+    return this
   }
-  divide () {
+  divide (inputDivide) {
+    this.result = this.result / inputDivide
+    this.tampilan = `hasil kalkulasi adalah ${this.result}`
+    return this
   }
-  square () {
+  square (inputSquare) {
+    this.result = Math.pow(this.result, inputSquare)
+    this.tampilan = `hasil kalkulasi adalah ${this.result}`
+    return this
   }
   squareRoot () {
+    this.result = Math.sqrt(this.result)
+    this.tampilan = `hasil kalkulasi adalah ${this.result}`
+    return this
   }
+
+  phi(){
+    let phiNumber =  3.14
+    return phiNumber
+  }
+  
+  // bulatkan(input){
+  //   this.result = this.result.toFixed(input)
+  //   this.tampilan = `hasil kalkulasi adalah ${this.result}`
+    
+  // }
 }
+
+const testing = new Calculator(10)
+
+// console.log(testing.add(5))
+// console.log(testing.multiply(2))
+// console.log(testing.add(5).multiply(2).divide(3).square(2).multiply(4).squareRoot())
+// console.log(testing.add(5).multiply(2).divide(3).square(2).multiply(4).squareRoot().add(testing.phi()))
 
 /** note : you can use several features from ecmascript, such as:
 * - Classes
@@ -27,3 +69,4 @@ class Calculator {
 */
 
 module.exports = Calculator
+
--- main.js
+++ main.js
@@ -3,3 +3,9 @@
 const Calculator = require("./calculator.js")
 
 // execute function on calculator.js in here
+
+const Calculate = new Calculator(25)
+
+console.log(Calculate.add(25).square(2).divide(125).divide(4).multiply(Calculate.phi()))
+// Calculate.add(25).square(2).divide(125).divide(4).multiply(Calculate.phi()).bulatkan(2)
+// console.log(Calculate)
