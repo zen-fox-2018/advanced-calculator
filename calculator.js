cc54312b979fbda63074b0b68b633edb2fcdd900
ChristianHS23 <christian.sihotang23@gmail.com>
2018-12-03 14:08:45 +0700

    Calculator.js Done

--- calculator.js
+++ calculator.js
@@ -2,22 +2,44 @@
 
 class Calculator {
   //write your code here
-  constructor () {
+  constructor (total) {
+    this.total = total
+    
+  } 
+  add (input) {
+    this.total += input
+    return this
   }
-  add () {
+  subtract (input) {
+    this.total -= input
+    return this
   }
-  subtract () {
+  multiply (input) {
+    this.total *= input
+    return this
   }
-  multiply () {
+  divide (input) {
+    this.total /= input
+    return this
   }
-  divide () {
-  }
-  square () {
+  square (input) {
+    // for(let i = 0; i < this.input; i++){
+    //   this.total *= this.total
+    // }
+    this.total = Math.pow(this.total, input)
+    return this
   }
   squareRoot () {
+    this.total = Math.sqrt(this.total)
+    return this
+  }
+  Pi(){
+    this.total *= (22/7)
+    return this
   }
 }
-
+// const test = new Calculator(64,5)
+// console.log(test.squareRoot())
 /** note : you can use several features from ecmascript, such as:
 * - Classes
 * - Default Parameters
--- main.js
+++ main.js
@@ -3,3 +3,9 @@
 const Calculator = require("./calculator.js")
 
 // execute function on calculator.js in here
+
+
+const newCalculator = new Calculator(2).add(2).multiply(3).square(2).divide(2).subtract(8).squareRoot().total // input pertama
+console.log(newCalculator)
+// (Akar((0 + 2 )*3)^2)*Pi
+//console.log(newCalculator.add(2).multiply(3).square(2).squareRoot().Pi().total)
